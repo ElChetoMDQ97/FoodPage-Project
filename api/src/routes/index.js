1e0b1e8be73b181888905e8825a308ec8f3fd8c3
ElChetoMDQ97 <braianher5@gmail.com>
2022-10-07 10:27:31 -0300

    v1.0.0A

---
+++ api/src/routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+
+// https://api.spoonacular.com/recipes/complexSearch?apiKey=f1a049603c98464ab6dddd0cfc82a9a6&addRecipeInformation=true
+
+
+
+module.exports = router;
